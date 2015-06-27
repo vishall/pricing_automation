d43bd1a17b2074ef6a69d136baa53e33919772f2
vishall <vishal.loke@cognizant.com>
2015-06-27 22:25:50 +0100

    VK| Pricing automation first commit

---
+++ src/main/groovy/com/pa/ExcelPriceReader.groovy
@@ -0,0 +1,41 @@
+package com.pa
+
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.xssf.usermodel.XSSFRow
+import org.apache.poi.xssf.usermodel.XSSFSheet
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
+
+class ExcelPriceReader {
+    static XSSFRow row;
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = new FileInputStream(
+                new File("/home/user/Downloads/Recommended Tariff 2606.xlsx"));
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet spreadsheet = workbook.getSheetAt(0);
+        Iterator<Row> rowIterator = spreadsheet.iterator();
+        while (rowIterator.hasNext()) {
+            row = (XSSFRow) rowIterator.next();
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                switch (cell.getCellType()) {
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print(
+                                cell.getNumericCellValue() + " \t\t ");
+                        break;
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(
+                                cell.getStringCellValue() + " \t\t ");
+                        break;
+                }
+            }
+            System.out.println();
+        }
+        fis.close();
+    }
+
+
+}
---
+++ src/test/groovy/ExcelPriceReaderTest.groovy
@@ -0,0 +1,5 @@
+/**
+ * Created by user on 27/06/15.
+ */
+class ExcelPriceReaderTest {
+}
